e8b4822484330e5f733b019550233d64ac11751b
szaccagni <sarahzaccagni@gmail.com>
2023-06-02 13:08:52 -0400

    trying to resolve sticky photos

--- src/QuizCard/QuizCard.jsx
+++ src/QuizCard/QuizCard.jsx
@@ -9,17 +9,22 @@ export default function QuizCard({ setShowResult, setShowQuiz, setGenres, getMov
     const [curQuestion, setCurQuestion] = useState(0)
     const [responses, setResponses] = useState({})
     const [selected, setSelected] = useState('')
+    const [question, setQuestion] = useState(data[curQuestion])
 
     const disabled = curQuestion === 0 ? true : false
 
     function handleBack() {
         setCurQuestion(curQuestion - 1)
         setSelected(responses[curQuestion - 1])
+        setQuestion('')
+        setQuestion(data[curQuestion - 1])
     }
 
     function handleNext() {
         if (curQuestion + 1 < data.length) {
             setCurQuestion(curQuestion + 1)
+            setQuestion('')
+            setQuestion(data[curQuestion + 1])
             responses[curQuestion + 1] ? setSelected(responses[curQuestion + 1]) : setSelected('')
         } else {
             setSelected('')
@@ -43,42 +48,47 @@ export default function QuizCard({ setShowResult, setShowQuiz, setGenres, getMov
     const progress = (curQuestion / data.length) * 100;
 
     return (
-        <div className='quiz'>
-            <div className="question-container"><div>{data[curQuestion].question}</div></div>
-            <LinearProgress variant="determinate" value={progress} id="progress-bar"/>
-            <div className="response-container">
-                {data[curQuestion].response.map((option, idx) => (
-                    <ResponseCard
-                        key={idx}
-                        option={option}
-                        selected={selected}
-                        setSelected={setSelected}
-                        responses={responses}
-                        setResponses={setResponses}
-                        curQuestion={curQuestion}
-                    />
-                ))}
+        <>
+        {
+            question.response &&
+            <div className='quiz'>
+                <div className="question-container"><div>{question.question}</div></div>
+                <LinearProgress variant="determinate" value={progress} id="progress-bar"/>
+                <div className="response-container">
+                    {question.response.map((option, idx) => (
+                        <ResponseCard
+                            key={idx}
+                            option={option}
+                            selected={selected}
+                            setSelected={setSelected}
+                            responses={responses}
+                            setResponses={setResponses}
+                            curQuestion={curQuestion}
+                        />
+                    ))}
 
-                <div className="next-btn-container">
-                    <Button
-                        id="back-btn"
-                        variant="outlined"
-                        onClick={handleBack}
-                        disabled={disabled}
-                    >
-                        Back
-                    </Button>
-                    {selected !== '' &&
+                    <div className="next-btn-container">
                         <Button
-                            id="next-btn"
-                            onClick={handleNext}
-                            variant="contained"
+                            id="back-btn"
+                            variant="outlined"
+                            onClick={handleBack}
+                            disabled={disabled}
                         >
-                            next
-                        </Button>}
+                            Back
+                        </Button>
+                        {selected !== '' &&
+                            <Button
+                                id="next-btn"
+                                onClick={handleNext}
+                                variant="contained"
+                            >
+                                next
+                            </Button>}
+                    </div>
                 </div>
             </div>
-        </div>
+        }
+        </>
     )
 
 }
